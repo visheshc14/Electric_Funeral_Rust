5cf03600d8de857e65f1ab9a05c0652defd80ae6
Vishesh Choudhary <36515357+visheshc14@users.noreply.github.com>
2021-02-19 02:01:15 +0530

    Updating Return Parameter

--- dos-attack/src/main.rs
+++ dos-attack/src/main.rs
@@ -1,17 +1,4 @@
-// Copyright (C) 2020 Cody Lewis
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 extern crate pnet;
 
@@ -20,9 +7,9 @@ use std::env;
 mod icmp_flood;
 
 fn main() {
-    println!("dos-attacks  Copyright (C) 2020  Cody Lewis");
+    println!("Sequential-dos-attacks in Rust.");
     println!("This program comes with ABSOLUTELY NO WARRANTY.");
-    println!("This is free software, and you are welcome to redistribute it under certain conditions.");
+    println!("You can make a PR for this.");
     println!();
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
